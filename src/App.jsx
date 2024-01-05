919bccfe0317324baf408d61d877745998d3e412
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-01-06 03:55:26 +0900

    [feat] 메인페이지 리스트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main.jsx";
 import Login from "./pages/Login.jsx";
+import Topic from "./pages/Topic.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />}></Route>
+        <Route path="/topic" element={<Topic />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
     </Router>
