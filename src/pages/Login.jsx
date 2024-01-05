1da2517c653bc922a9cdd6c14660959a484171e2
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-01-05 22:12:40 +0900

    [feat] 로그인 기본 틀 구현

---
+++ src/pages/Login.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Login = () => {
+  const [LoginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <PageComponent>
+      <FlexBox>
+        <LoginTotalComponent>
+          <LoginHeader>Login</LoginHeader>
+          <LoginInput placeholder="아이디를 입력해주세요." />
+          <LoginInput placeholder="비밀번호를 입력해주세요." />
+        </LoginTotalComponent>
+      </FlexBox>
+    </PageComponent>
+  );
+};
+
+export default Login;
+
+const PageComponent = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const FlexBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginTotalComponent = styled.div`
+  width: 700px;
+  height: 400px;
+  border: 1px solid black;
+`;
+
+const LoginHeader = styled.h3`
+  font-size: 20px;
+`;
+
+const LoginInput = styled.input`
+  width: 280px;
+  height: 40px;
+  display: block;
+`;
