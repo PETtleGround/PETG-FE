4cb122ab2b24e80a6fc5f4722d673ef305829fa9
MinJaeSon <mjsonsj@gmail.com>
2024-01-06 03:57:14 +0900

    [chore] 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main.jsx";
 import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import SignUpSuccess from "./pages/SignUpSuccess.jsx";
+import { GlobalStyle } from "./theme/GlobalStyle.jsx";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-    </Router>
+    <div>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signedup" element={<SignUpSuccess />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
