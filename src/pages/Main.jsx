d8dbc2dc471b7d71ac0765911a86b09b9524ec27
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-01-06 00:49:09 +0900

    [feat] 로그인 화면api 제외  완료

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,18 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import styled from "styled-components";
 
 const Main = () => {
-  return <div>메인페이지</div>;
+  return (
+    <PageComponent>
+      <div>메뉴바</div>
+    </PageComponent>
+  );
 };
 
 export default Main;
+
+const PageComponent = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
