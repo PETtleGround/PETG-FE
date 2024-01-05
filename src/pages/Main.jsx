b6d5fa3928b654711c80102c66a633087a5c7770
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-01-06 02:50:27 +0900

    [feat] 메인페이지 슬릭 제외 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,67 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import PETG_img from "../assets/PETG.svg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import trophy_img from "../assets/trophy.svg";
 
 const Main = () => {
-  const [petList, setPetList] = useState("");
-
-  const getPetList = async () => {
-    // try {
-    //   await UserLogin(loginInfo).then((res) => {
-    //     console.log(res);
-    //   });
-    // } catch (err) {
-    //   console.log(err);
-    // }
-    setPetList([
-      {
-        img: PETG_img,
-        title: "어떤 브랜드가 살아남는가? 인정받는 브랜드의 비밀",
-        content: "내용 예시입니당",
-      },
-      {
-        img: PETG_img,
-        title: "어떤 브랜드가 살아남는가? 인정받는 브랜드의 비밀",
-        content: "내용 예시입니당",
-      },
-      {
-        img: PETG_img,
-        title: "어떤 브랜드가 살아남는가? 인정받는 브랜드의 비밀",
-        content: "내용 예시입니당",
-      },
-      {
-        img: PETG_img,
-        title: "어떤 브랜드가 살아남는가? 인정받는 브랜드의 비밀",
-        content: "내용 예시입니당",
-      },
-      {
-        img: PETG_img,
-        title: "어떤 브랜드가 살아남는가? 인정받는 브랜드의 비밀",
-        content: "내용 예시입니당",
-      },
-      {
-        img: PETG_img,
-        title: "어떤 브랜드가 살아남는가? 인정받는 브랜드의 비밀",
-        content: "내용 예시입니당",
-      },
-      {
-        img: PETG_img,
-        title: "어떤 브랜드가 살아남는가? 인정받는 브랜드의 비밀",
-        content: "내용 예시입니당",
-      },
-    ]);
-  };
-
   return (
     <PageComponent>
       <div>메뉴바</div>
       <MainHeader>
         <Image src={PETG_img} alt="PETG" />
-        <NanumTextDiv margin="-24px 0px 0px 0px">핫한 토픽 둘러보기</NanumTextDiv>
+        <NanumTextDiv margin="-24px 0px 0px 0px">토픽 TOP3</NanumTextDiv>
         <InterTextDiv>지금 바로 나의 PETG를 저장해보세요</InterTextDiv>
-        <InterTextDiv margin="7px 0px 0px 0px">현재 인기 많은 아이는?</InterTextDiv>
+        <InterTextDiv margin="7px 0px 0px 0px">현재 인기 많은 PETG는?</InterTextDiv>
       </MainHeader>
+      {/* slick 슬라이드 */}
+
+      <ButtonDiv margin="95px 0px 0px 0px">모든 토픽 보러가기</ButtonDiv>
+      <TrophyImage width="97.656px" height="94px" src={trophy_img} alt="trophy" />
     </PageComponent>
   );
 };
@@ -71,6 +30,9 @@ export default Main;
 const PageComponent = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const MainHeader = styled.div`
@@ -78,7 +40,6 @@ const MainHeader = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 50px 0px 0px 0px;
 `;
 const Image = styled.img`
   width: ${(props) => props.width || "476px"};
@@ -87,6 +48,18 @@ const Image = styled.img`
   display: block;
   z-index: -1;
 `;
+
+const TrophyImage = styled.img`
+  position: absolute;
+  width: ${(props) => props.width || "476px"};
+  height: ${(props) => props.height || "236px"};
+  object-fit: contain;
+  display: block;
+  z-index: -1;
+  bottom: 77px;
+  right: 56.34px;
+`;
+
 const NanumTextDiv = styled.div`
   color: var(--Text-1, #1e1e1e);
   font-family: NanumSquareRound;
@@ -107,5 +80,24 @@ const InterTextDiv = styled.div`
   font-weight: 400;
   line-height: normal;
   font-size: ${(props) => props.fontSize || "20px"};
+  margin: ${(props) => props.margin || "0px"};
+`;
+
+const ButtonDiv = styled.button`
+  display: flex;
+  width: 600px;
+  height: 58px;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  border-radius: 12px;
+  background: var(--Main, #ec8007);
+  color: var(--White, #fff);
+  font-family: NanumSquareRound;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 160%; /* 25.6px */
+  border: none;
   margin: ${(props) => props.margin || "0px"};
 `;
